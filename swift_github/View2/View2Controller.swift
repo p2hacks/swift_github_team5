b4ba37bc67891010f3ac3a550fadf551cadaebfd
ARATAYOKOYAMA <bob2525@hotmail.co.jp>
2018-12-11 17:02:38 +0900

    add ignore

---
+++ swift_github/View2/View2Controller.swift
@@ -0,0 +1,30 @@
+//
+//  View2Controller.swift
+//  swift_github
+//
+//  Created by 横山新 on 2018/12/11.
+//  Copyright © 2018 P2HACKS. All rights reserved.
+//
+
+import UIKit
+
+class View2Controller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ swift_github/swift_github/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift_github
+//
+//  Created by 横山新 on 2018/12/11.
+//  Copyright © 2018 P2HACKS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
